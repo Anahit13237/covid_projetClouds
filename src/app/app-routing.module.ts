02565c4bcc34d0eb787753b0414ac2dfeb0a5640
Anahit13237 <77056798+Anahit13237@users.noreply.github.com>
2021-01-27 01:03:53 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NewsComponent } from './news/news.component';
+import { SecurePagesGuard } from './secure-pages.guard';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "homepage", component: HomepageComponent},
+  {path : "signin", component: SigninComponent,canActivate:[SecurePagesGuard]},
+  /* the canactivate ADD the condition that a user can not get to page news if he has not signed in*/
+  {path : "news", component: NewsComponent, canActivate:[AuthGuard]},
+  {path : "", pathMatch:"full", redirectTo: "homepage"},
+  {path : "**", redirectTo: "homepage"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
